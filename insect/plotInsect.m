44293d0db4208b3df6308233997cd9e8f6be79e8
unknown <TIM2060037@MC865790-111-09.ad.mc.local>
2015-10-06 20:29:20 -0700

    Update insect plot

--- insect/plotInsect.m
+++ insect/plotInsect.m
@@ -1,7 +1,22 @@
-function plotInsect( dimen )
+function plotInsect()
 
-fplot('offsetContainers(50, 30, 100, x)', [dimen(1), dimen(2), dimen(3), dimen(4)]);
+figure(1);
+fplot('offsetContainers(50, 30, 100, x)', [1.78, 1.79, 115, 125]);
 title('Case 1');
+xlabel('radius');
+ylabel('number of containers');
+grid on;
+
+figure(2);
+fplot('offsetContainers(100, 50, 30, x)', [4.6, 4.7, 115, 125]);
+title('Case 2');
+xlabel('radius');
+ylabel('number of containers');
+grid on;
+
+figure(3);
+fplot('offsetContainers(30, 100, 50, x)', [3.567, 3.576, 110, 115]);
+title('Case 3');
 xlabel('radius');
 ylabel('number of containers');
 grid on;
